5407eb0cc39cfe9aedb8955990bf892aad911ef9
deive salazar lopez <deive1354@gmail.com>
2018-05-31 19:46:43 -0500

    base

---
+++ Principal/MDI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Principal
+{
+    public partial class MDI : Form
+    {
+        public MDI()
+        {
+            InitializeComponent();
+        }
+
+        private void salirDelSistemaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OpSocios_Click(object sender, EventArgs e)
+        {
+            var frm_Socios = new Socios(ref this.OpSocios) { MdiParent = this , WindowState= FormWindowState.Maximized};
+ 
+
+            frm_Socios.Show();
+
+        }
+
+
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MDI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
