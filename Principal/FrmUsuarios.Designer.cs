257fe5658b3ac5653a72e40bb582664cfb55122a
deive salazar lopez <deive1354@gmail.com>
2018-06-07 21:22:48 -0500

    cambios D

--- Principal/FrmUsuarios.Designer.cs
+++ Principal/FrmUsuarios.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUsuarios));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtnodeusuario = new System.Windows.Forms.TextBox();
+            this.txtcontraseña = new System.Windows.Forms.TextBox();
+            this.txtusuarios = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lavell = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkactivo = new System.Windows.Forms.CheckBox();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.OpSalir = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
@@ -38,8 +47,83 @@
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.OpCancelar = new System.Windows.Forms.ToolStripButton();
             this.OpGuardar = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-25, 145);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(628, 131);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // txtnodeusuario
+            // 
+            this.txtnodeusuario.Location = new System.Drawing.Point(103, 103);
+            this.txtnodeusuario.Name = "txtnodeusuario";
+            this.txtnodeusuario.Size = new System.Drawing.Size(100, 20);
+            this.txtnodeusuario.TabIndex = 21;
+            // 
+            // txtcontraseña
+            // 
+            this.txtcontraseña.Location = new System.Drawing.Point(103, 77);
+            this.txtcontraseña.Name = "txtcontraseña";
+            this.txtcontraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtcontraseña.TabIndex = 20;
+            // 
+            // txtusuarios
+            // 
+            this.txtusuarios.Location = new System.Drawing.Point(103, 51);
+            this.txtusuarios.Name = "txtusuarios";
+            this.txtusuarios.Size = new System.Drawing.Size(100, 20);
+            this.txtusuarios.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "No de usuario";
+            // 
+            // lavell
+            // 
+            this.lavell.AutoSize = true;
+            this.lavell.Location = new System.Drawing.Point(24, 84);
+            this.lavell.Name = "lavell";
+            this.lavell.Size = new System.Drawing.Size(60, 13);
+            this.lavell.TabIndex = 17;
+            this.lavell.Text = "contraseña";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Usuarios";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "activo";
+            // 
+            // checkactivo
+            // 
+            this.checkactivo.AutoSize = true;
+            this.checkactivo.Location = new System.Drawing.Point(396, 44);
+            this.checkactivo.Name = "checkactivo";
+            this.checkactivo.Size = new System.Drawing.Size(15, 14);
+            this.checkactivo.TabIndex = 14;
+            this.checkactivo.UseVisualStyleBackColor = true;
             // 
             // toolStrip1
             // 
@@ -55,7 +139,7 @@
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(579, 25);
-            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.TabIndex = 13;
             this.toolStrip1.Text = "toolStrip1";
             // 
             // OpSalir
@@ -65,7 +149,6 @@
             this.OpSalir.Name = "OpSalir";
             this.OpSalir.Size = new System.Drawing.Size(49, 22);
             this.OpSalir.Text = "Salir";
-            this.OpSalir.Click += new System.EventHandler(this.OpSalir_Click);
             // 
             // toolStripSeparator1
             // 
@@ -124,10 +207,20 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(579, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtnodeusuario);
+            this.Controls.Add(this.txtcontraseña);
+            this.Controls.Add(this.txtusuarios);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lavell);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkactivo);
             this.Controls.Add(this.toolStrip1);
             this.Name = "FrmUsuarios";
             this.Text = "FrmUsuarios";
             this.Load += new System.EventHandler(this.FrmUsuarios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -137,6 +230,15 @@
 
         #endregion
 
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtnodeusuario;
+        private System.Windows.Forms.TextBox txtcontraseña;
+        private System.Windows.Forms.TextBox txtusuarios;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lavell;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkactivo;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton OpSalir;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
