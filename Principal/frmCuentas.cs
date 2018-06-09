541c684c168867cde88aaa148215f2c7adce80f2
Omar Gonzalez <omarmonoxdgmail.com>
2018-06-08 23:53:03 -0500

    Cambios

--- Principal/frmCuentas.cs
+++ Principal/frmCuentas.cs
@@ -12,8 +12,12 @@ namespace Principal
 {
     public partial class frmCuentas : Form
     {
-         ToolStripButton _BotonLocal;
-         
+        ToolStripButton _BotonLocal;
+
+        enum Accion { Agregando, Modificando, Eliminando, Nada };
+        Accion acc = Accion.Nada;
+
+
         public frmCuentas(ref ToolStripButton BotonMDI)
         {
             InitializeComponent();
@@ -22,17 +26,131 @@ namespace Principal
 
         }
 
-        private void frmCuentas_Load(object sender, EventArgs e)
+        private void Cuentas_FormClosing(object sender, FormClosingEventArgs e)
         {
+            this._BotonLocal.Enabled = true;
+        }
+
+        private void Cuentas_Load(object sender, EventArgs e)
+        {
+            this._BotonLocal.Enabled = false;
+
+
+            Carga_Grid();
 
         }
 
-        private void OpSalir_Click(object sender, EventArgs e)
+
+
+        private void Carga_Grid()
+        {
+            var lista = from x in new dbCAJA().Cuentas
+                        select x;
+            this.dataGridView1.DataSource = lista.ToList();
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
         {
             this.Close();
+
         }
 
-        private void OpSalir_Click_1(object sender, EventArgs e)
+        private void frmCuentas_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void OpAgregar_Click(object sender, EventArgs e)
+        {
+            
+
+            foreach (Control x in this.Controls)
+            {
+                x.Visible = true;
+                if (x.GetType().Name == "TextBox")
+                {
+                    x.Text = "";
+                }        
+            }
+            OpSalir.Enabled = !false;
+            OpAgregar.Enabled = false;
+            OpModificar.Enabled = false;
+            OpEliminar.Enabled = false;
+            OpCancelar.Enabled = true;
+            OpGuardar.Enabled = true;
+            acc = Accion.Agregando;
+
+        }
+
+
+
+
+
+
+        /*
+          foreach (Control n in CO.Controls)
+                    {
+                        if ( n.GetType().Name.Equals(p_control))
+                        {
+                            n.Text = string.Empty;
+                        }
+                        
+                    }
+        */
+
+
+        private void OpGuardar_Click(object sender, EventArgs e)
+        {
+            if (IdSocio.Text == "" || NoCuenta.Text == "" || IdCuenta.Text == "")
+            {
+                MessageBox.Show("No deje campos en blanco");
+                return;
+            }
+
+
+
+            if (acc == Accion.Agregando)
+
+
+            CancelaAccion();
+            Carga_Grid();
+
+
+        }
+
+        private void OpCancelar_Click(object sender, EventArgs e)
+        {
+            CancelaAccion();
+        }
+
+
+        private void CancelaAccion()
+        {
+            foreach (Control x in this.Controls)
+            {
+
+                if (x.GetType().Name == "TextBox")
+                {
+                    x.Text = "";
+                    x.Visible = false;
+                }
+
+                if (x.GetType().Name == "Label") x.Visible = false;
+                if (x.GetType().Name == "TextBox") x.Visible = false;
+
+
+
+            }
+            OpSalir.Enabled = !false;
+            OpAgregar.Enabled = !false;
+            OpModificar.Enabled = !false;
+            OpEliminar.Enabled = !false;
+            OpCancelar.Enabled = !true;
+            OpGuardar.Enabled = !true;
+            acc = Accion.Nada;
+        }
+
+        private void OpSalir_Click1(object sender, EventArgs e)
         {
             this.Close();
         }
